b0c20b372b4281a9d7d131add1b87320998e3529
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-12-27 11:46:37 +0700

    feat: add pagination feature

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -40,7 +40,9 @@
         </div>
     </div>
 
-    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+    {{ $posts->links() }}
+
+    <div class="my-4 py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
         <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
             @forelse ($posts as $post)
                 <article
@@ -94,5 +96,7 @@
             @endforelse
         </div>
     </div>
+
+    {{ $posts->links() }}
 
 </x-layout>
